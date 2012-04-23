e1f54c27267fe9157cc6353caf8d52cd9634b384
Hans-Jakob Bollinger <contact@webbear.ch>
2012-04-23 10:27:29 +0200

    updated german files

---
+++ ion_auth_lang.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - English
+* 
+* Author: Ben Edmunds
+* 		  ben.edmunds@gmail.com
+*         @benedmunds
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  03.14.2010 
+* 
+* Description:  English language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Account erfolgreich erstellt';
+$lang['account_creation_unsuccessful'] 	 	 = 'Der Account konnte nicht erstellt werden';
+$lang['account_creation_duplicate_email'] 	 = 'Email ist entweder ungültig oder schon in Gebrauch';
+$lang['account_creation_duplicate_username'] 	 = 'Der Benutzername ist entweder ungültig oder schon in Gebrauch';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Passwort erfolgreich geändert';
+$lang['password_change_unsuccessful'] 	  	 = 'Das Passwort konnte nicht geändert werden';
+$lang['forgot_password_successful'] 	 	 = 'Passwort-Reset-Email wurde versandt';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Das Passwort konnte nicht zurückgesetzt werden';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Account aktiviert';
+$lang['activate_unsuccessful'] 		 	 = 'Der Account konnte nicht aktiviert werden';
+$lang['deactivate_successful'] 		  	 = 'Account deaktiviert';
+$lang['deactivate_unsuccessful'] 	  	 = 'Der Account konnte nicht deaktiviert werden';
+$lang['activation_email_successful'] 	  	 = 'Activatierungs-Email wurde versandt';
+$lang['activation_email_unsuccessful']   	 = 'Die Aktivierungs-Email konnte nicht versandt werden';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Erfolgreich angemeldet';
+$lang['login_unsuccessful'] 		  	 = 'Falsches Login';
+$lang['logout_successful'] 		 	 = 'Erfolgreich abgemeldet.';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Account Informationen erfolgreich aktualisiert';
+$lang['update_unsuccessful'] 		 	 = 'Die Account Informationen konnten nicht aktualisert werden';
+$lang['delete_successful'] 		 	 = 'Benutzer gelöscht';
+$lang['delete_unsuccessful'] 		 	 = 'Der Benutzer konnte nicht gelöscht werden';
